afa079a3095f6d1b468a6333078a34192f77d469
jacobjshelp <jacobjshelp>
2025-01-11 09:18:09 +0400

    step  8 - Login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './styles/App.css'
-import AvailableGroups from './components/AvailableGroups'
 import { UserContext, UserInfo } from './contexts/UserContext'
 import { useState } from 'react'
+import { AuthenticationLayer } from './components/AuthenticationLayer'
 
 function App() {
   const [info, setInfo] = useState<UserInfo | null>(null)
@@ -12,7 +12,7 @@ function App() {
   return (
     <UserContext.Provider value={{ info, setInfo }}>
       <QueryClientProvider client={queryClient}>
-        <AvailableGroups />
+        <AuthenticationLayer />
       </QueryClientProvider>
     </UserContext.Provider>
   )
