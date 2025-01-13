6cff39a92541f50565541950fdb0d81f4ac79e27
jacobjshelp <jacobjshelp>
2025-01-13 18:29:03 +0400

    step 14 - layout and logout

--- frontend/src/components/AuthenticationLayer.tsx
+++ frontend/src/components/AuthenticationLayer.tsx
@@ -1,10 +1,16 @@
 import { useContextAndErrorIfNull, UserContext } from '../contexts/UserContext'
 import Authenticated from './Authenticated'
+import { Layout } from './Layout'
 import Login from './Login'
 
 export function AuthenticationLayer() {
   const { info } = useContextAndErrorIfNull(UserContext)
 
-  if (info) return <Authenticated />
+  if (info)
+    return (
+      <Layout>
+        <Authenticated />
+      </Layout>
+    )
   return <Login />
 }
